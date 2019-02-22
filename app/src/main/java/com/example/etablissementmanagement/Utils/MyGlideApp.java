608d851728c542222d69ca1636e4752d651f7252
AbdoBoum <abderrahim.boumahdi@um5s.net.ma>
2019-02-23 00:27:33 +0100

    Adding shared preference for user authentication

--- app/src/main/java/com/example/etablissementmanagement/Utils/MyGlideApp.java
+++ app/src/main/java/com/example/etablissementmanagement/Utils/MyGlideApp.java
@@ -3,6 +3,7 @@ package com.example.etablissementmanagement.Utils;
 import com.bumptech.glide.annotation.GlideModule;
 import com.bumptech.glide.module.AppGlideModule;
 
+
 @GlideModule
 public class MyGlideApp extends AppGlideModule {
 
